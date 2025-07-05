e725d312868b2074a13da38ce7b17793ca3d9ebb
Juanma197 <juanmaest197@gmail.com>
2025-07-05 23:34:20 +0100

    Update drive_utils.py

--- drive_utils.py
+++ drive_utils.py
@@ -1,32 +1,70 @@
-def get_or_create_folder(drive, parent_folder_id, folder_name):
-    query = f"'{parent_folder_id}' in parents and trashed=false and title='{folder_name}' and mimeType='application/vnd.google-apps.folder'"
-    file_list = drive.ListFile({'q': query}).GetList()
-    if file_list:
-        return file_list[0]['id']
-    else:
-        folder_metadata = {
-            'title': folder_name,
-            'mimeType': 'application/vnd.google-apps.folder',
-            'parents': [{'id': parent_folder_id}]
-        }
-        folder = drive.CreateFile(folder_metadata)
-        folder.Upload()
-        return folder['id']
+import os
+import json
+import tempfile
+from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import streamlit as st
 
+@st.cache_resource
+def connect_to_drive():
+    scope = ['https://www.googleapis.com/auth/drive']
+    credentials_dict = dict(st.secrets["gdrive_service_account"])
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
+    gauth = GoogleAuth()
+    gauth.credentials = credentials
+    drive = GoogleDrive(gauth)
+    return drive
 
-def upload_file_to_drive(folder_id, local_file_path, new_filename=None):
-    file_drive = drive.CreateFile({'parents': [{'id': folder_id}]})
-    file_drive['title'] = new_filename or os.path.basename(local_file_path)
-    file_drive.SetContentFile(local_file_path)
+def get_or_create_folder(drive, name, parent_id=None):
+    query = f"title='{name}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
+    if parent_id:
+        query += f" and '{parent_id}' in parents"
+    folders = drive.ListFile({'q': query}).GetList()
+    if folders:
+        return folders[0]['id']
+    metadata = {'title': name, 'mimeType': 'application/vnd.google-apps.folder'}
+    if parent_id:
+        metadata['parents'] = [{'id': parent_id}]
+    folder = drive.CreateFile(metadata)
+    folder.Upload()
+    return folder['id']
+
+def ensure_property_structure(drive, property_name):
+    month = datetime.today().strftime("%Y-%m")
+    base_id = get_or_create_folder(drive, "AccountingHQ")
+    prop_id = get_or_create_folder(drive, property_name, base_id)
+
+    folder_ids = {}
+    for sub in ["Income", "Expenses", "Invoices", "Salary", "Backups"]:
+        sub_id = get_or_create_folder(drive, sub, prop_id)
+        month_id = get_or_create_folder(drive, month, sub_id)
+        folder_ids[sub] = month_id
+    return folder_ids
+
+def upload_file_to_drive(drive, folder_id, local_path, filename=None):
+    if not filename:
+        filename = os.path.basename(local_path)
+    file_drive = drive.CreateFile({'title': filename, 'parents': [{'id': folder_id}]})
+    file_drive.SetContentFile(local_path)
     file_drive.Upload()
     return file_drive['id']
 
-
-def list_files(folder_id):
+def list_files(drive, folder_id):
     query = f"'{folder_id}' in parents and trashed=false"
     return drive.ListFile({'q': query}).GetList()
 
+def download_file(drive, file_id, local_path):
+    file_drive = drive.CreateFile({'id': file_id})
+    file_drive.GetContentFile(local_path)
 
-def download_file(file_id, save_path):
-    file = drive.CreateFile({'id': file_id})
-    file.GetContentFile(save_path)
+def backup_locally(local_path, backup_folder="backups"):
+    os.makedirs(backup_folder, exist_ok=True)
+    base = os.path.basename(local_path)
+    dst = os.path.join(backup_folder, base)
+    with open(local_path, 'rb') as src_file:
+        with open(dst, 'wb') as dst_file:
+            dst_file.write(src_file.read())
+    return dst
+
