835f035669377dc38735f0af696707416a4385ae
Juanma197 <juanmaest197@gmail.com>
2025-07-05 23:06:15 +0100

    Update drive_utils.py

--- drive_utils.py
+++ drive_utils.py
@@ -1,20 +1,21 @@
 import json
+import tempfile
+from oauth2client.service_account import ServiceAccountCredentials
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 import streamlit as st
-from oauth2client.service_account import ServiceAccountCredentials
 
 @st.cache_resource
 def connect_to_drive():
-    # Load secrets from Streamlit's secrets.toml
+    scope = ['https://www.googleapis.com/auth/drive']
     credentials_dict = dict(st.secrets["gdrive_service_account"])
 
-    # Define required Drive API scope
-    scope = ["https://www.googleapis.com/auth/drive"]
-
-    # Authenticate using the service account JSON contents
+    # Create credentials object from secrets
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
 
+    # Authorize with PyDrive2
     gauth = GoogleAuth()
     gauth.credentials = credentials
-    return GoogleDrive(gauth)
+    drive = GoogleDrive(gauth)
+
+    return drive
