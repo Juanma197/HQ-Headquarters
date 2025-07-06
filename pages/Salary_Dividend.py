888c2f727b26445413aef918c9d23e7f1e648ffa
Juanma197 <juanmaest197@gmail.com>
2025-07-06 01:10:15 +0100

    Update Salary_Dividend.py

--- pages/Salary_Dividend.py
+++ pages/Salary_Dividend.py
@@ -2,7 +2,8 @@ import streamlit as st
 import json
 from datetime import datetime
 import tempfile
-from utils.drive_utils import connect_to_drive, ensure_property_structure, upload_file_to_drive, ...
+from utils.drive_utils import connect_to_drive, ensure_property_structure, upload_file_to_drive, backup_locally
+
 
 
 st.set_page_config(page_title="Salary & Dividend Simulator", layout="centered")
